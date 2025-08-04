57bb50221869364cc598deb79ec74e911f779421
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-08-04 18:14:49 -0400

    adding dockerignore to reduce image size

--- app/service/transcription.py
+++ app/service/transcription.py
@@ -6,6 +6,7 @@ from core.config import settings
 from core.logging import logger
 from models.whisper_model_base import model
 
+
 async def transcribe_audio(audio_data: bytes) -> dict:
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmpfile:
         with wave.open(tmpfile.name, "wb") as wf:
@@ -18,28 +19,32 @@ async def transcribe_audio(audio_data: bytes) -> dict:
             data, samplerate = sf.read(tmpfile.name)
             duration = len(data) / samplerate
             amplitude = float(np.max(np.abs(data)))
-            logger.info(f"Audio duration: {duration:.2f} sec, Max amplitude: {amplitude}")
+            logger.info(
+                f"Audio duration: {duration:.2f} sec, Max amplitude: {amplitude}"
+            )
             # Normalize the audio data
             data = data / np.max(np.abs(data))
             # Update the data with a normalized audio file.
-            sf.write(tmpfile.name, data, samplerate, subtype='PCM_16')
+            sf.write(tmpfile.name, data, samplerate, subtype="PCM_16")
         except Exception as e:
             logger.error(f"Error loading audio: {e}")
             return {"error": str(e)}
 
         logger.info(f"Transcribing {tmpfile.name}")
         result = model().transcribe(
-            tmpfile.name, 
-            language="en", 
+            tmpfile.name,
+            language="en",
             fp16=settings.DEVICE == "cuda",
             temperature=0.0,
-            word_timestamps=True ,
+            word_timestamps=True,
         )
         transcript = result.get("text", "").strip()
         segments = result.get("segments", [])
         confidence = segments[0].get("no_speech_prob", 0.0) if segments else 0.0
         avg_logprob = segments[0].get("avg_logprob", 0.0) if segments else 0.0
-        logger.info(f"Transcript: {transcript}, Confidence: {confidence}, Logprob: {avg_logprob}")
+        logger.info(
+            f"Transcript: {transcript}, Confidence: {confidence}, Logprob: {avg_logprob}"
+        )
 
         return {
             "transcript": transcript,
